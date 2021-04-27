79f84308882fa02f5d6014f52100706fa85507cd
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-04-27 12:59:33 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 function App() {
+  const Url = process.env.REACT_APP_API_URL;
+
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const setupSocket = () => {
+    const newSocket = io(`${Url}`);
+    newSocket.on("connect", () => {});
+    setSocket(newSocket);
+  };
+
+  useEffect(() => {
+    setupSocket();
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("reqMessage", (data) => {
+        setMessages([...messages, data]);
+      });
+    }
+  }, [socket, messages]);
+
+  const handleClick = () => {
+    socket.emit("sendMessage", message);
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-4 pt-4">
+          <h5 className="mb-4">Messages</h5>
+          <ul className="list-group">
+            {messages.map((item, index) => (
+              <li className="list-group-item" key={index}>
+                {item}
+              </li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            name="message"
+            id="message"
+            className={`form-control ${messages.length > 0 && "mt-4"}`}
+            value={message}
+            placeholder="Write your message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            type="button"
+            className="btn btn-primary mt-4"
+            onClick={handleClick}
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
